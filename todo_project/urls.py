012ee6653ea89f37add4f11e82ca1655a8d1e421
Surya Panday <suryapanday1.sp@outlook.com>
2025-08-22 15:46:36 +1000

    simple view added

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from todos.views import home
+
+urlpatterns = [path("admin/", admin.site.urls), path("", home, name="home")]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
